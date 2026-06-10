c50f70dd67c931c4d46f94ac3993437b22b721ae
huylnldg-cmyk <huyln.ldg@gmail.com>
2026-06-10 14:48:17 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // 3. Copy anon public key vào SUPABASE_ANON_KEY.
 // 4. Không dùng service_role key ở đây.
 
-window.SUPABASE_URL = https://owlznfkdlwvyxxixhueh.supabase.co
-window.SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93bHpuZmtkbHd2eXh4aXhodWVoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDQ5MDM3NSwiZXhwIjoyMDk2MDY2Mzc1fQ.6hTGy206L4mQGNbnHVsOLEB5GZfdjOOwZPIL_zkwnfI
+window.SUPABASE_URL = "https://owlznfkdlwvyxxixhueh.supabase.co"
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93bHpuZmtkbHd2eXh4aXhodWVoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDQ5MDM3NSwiZXhwIjoyMDk2MDY2Mzc1fQ.6hTGy206L4mQGNbnHVsOLEB5GZfdjOOwZPIL_zkwnfI"
